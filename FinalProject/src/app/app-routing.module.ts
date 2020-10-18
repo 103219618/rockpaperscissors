19876f6d7c72cbef851f0baeaf3b407e1ff3d767
Syed Razvi <103219618@swin.edu.au>
2020-10-19 01:17:46 +1100

    redirect routes

--- FinalProject/src/app/app-routing.module.ts
+++ FinalProject/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ScoreComponent } from './routes/score/score.component';
+import { SelectmenuComponent } from './routes/selectmenu/selectmenu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "pick", component: SelectmenuComponent },
+  { path: "display", component: ScoreComponent },
+
+  {path: "**", redirectTo: "pick"}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
