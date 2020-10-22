e19a3a5c4b86570f61dce227d57a4183bf36fdb1
Syed Razvi <103219618@swin.edu.au>
2020-10-22 13:49:45 +1100

    gamecontroller

---
+++ game_api/Controllers/GameController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace game_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GameController : ControllerBase
+    {
+
+    }
+}
