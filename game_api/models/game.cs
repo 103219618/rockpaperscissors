cdb9379361255eeb3bbceeeef4e6a311985f8267
Syed Razvi <103219618@swin.edu.au>
2020-10-22 13:48:27 +1100

    game model

--- game_api/models/game.cs
+++ game_api/models/game.cs
@@ -10,6 +10,30 @@ namespace game_api.models
         public void GenerateComSelection()
         {
             string[] ArrayChoice = { "Rock", "Paper", "Scissors" };
+            Random random = new Random();
+            this.ComChoice = ArrayChoice[random.Next(0,3)];
+        }
+
+        public void PlayerSelection(string playerselection)
+        {
+            Player p1 = new Player(playerselection);
+            this.PlayerChoice = p1.PlayerChoice;
+        }
+
+        public void Results()
+        {
+            this.GenerateComSelection();
+            string combineSelections = this.PlayerChoice + this.ComChoice;
+            
+            if (combineSelections == "RockScissors"  || combineSelections == "PaperRock" || combineSelections == "ScissorsPaper") {
+            this.GameResult = "You Win!";
+            }
+            else if (combineSelections == "RockPaper"  || combineSelections == "ScissorsRock" || combineSelections == "PaperScissors") {
+            this.GameResult = "You Lose!";
+            }
+            else {
+            this.GameResult = "Draw";
+            }
         }
     }
 }
